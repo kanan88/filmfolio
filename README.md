f906cbb4c35f82adfed54924d9f93f3feb78d6e0
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-01-25 13:49:31 +0100

    implement initial setup with latest tailwindcss 4 + app theme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Welcome to 🎬 FilmFolio 🎥 👋
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="font-bold underline text-3xl">Hello world!</h1>
+    </>
+  );
+};
+
+export default App;
